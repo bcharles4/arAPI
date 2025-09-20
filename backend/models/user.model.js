da38de7b9572619e77a4fe4fd4af3f4dd2584592
bcharles4 <95261817+bcharles4@users.noreply.github.com>
2025-09-20 22:20:25 +0800

    asd

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -3,11 +3,12 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true, minLength: 5, maxLength: 100 },
     email: { type: String, required: true, unique: true },
-    studentId: {
-        type: String,
-        required: true,
-        match: [/^\d{14}$/, "Student ID must be 14 digits with letters"]
+   studentId: {
+  type: String,
+    required: true,
+    match: [/^[A-Za-z0-9]{10,14}$/, "Student ID must be 10 to 14 characters with letters and digits"]
     },
+
 
     password: { type: String, required: true },
     score: { type: String, default: "0" }, // Default score set to 0
